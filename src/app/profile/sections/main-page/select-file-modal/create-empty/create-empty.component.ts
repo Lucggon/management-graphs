7ea6588c6b95d2ffdef9c3e487461920cd2b680a
Lgg <lucas.garcia.gond@gmail.com>
2024-02-04 20:13:34 +0100

    [FEATURE] start to add form for create empty data

--- src/app/profile/sections/main-page/select-file-modal/create-empty/create-empty.component.ts
+++ src/app/profile/sections/main-page/select-file-modal/create-empty/create-empty.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-empty',
   templateUrl: './create-empty.component.html',
   styleUrls: ['./create-empty.component.css']
 })
-export class CreateEmptyComponent {
+export class CreateEmptyComponent implements OnInit {
+  public createEmptyForm!: FormGroup;
 
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+      this.createEmptyForm = this.fb.group({
+        name: '',
+        columns: this.fb.array([this.buildColumnName])
+      })
+  }
+
+  get columns(): FormArray {
+    return this.createEmptyForm.get('columns') as FormArray;
+  }
+
+  buildColumnName(): FormGroup {
+      return this.fb.group({
+        column: ''
+      })
+  }
 }
